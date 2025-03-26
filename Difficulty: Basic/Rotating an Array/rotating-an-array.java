ed976d7b480db05300b024e508c792ec5a102890
Varshini-1491 <varshinisnigdha5@gmail.com>
2025-03-26 13:21:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Rotating an Array/rotating-an-array.java
+++ Difficulty: Basic/Rotating an Array/rotating-an-array.java
@@ -37,6 +37,7 @@ System.out.println("~");
 class Solution {
     void leftRotate(int arr[], int d) {
         int n=arr.length;
+        d=d%n;
         reverse(arr,0,d-1);
         reverse(arr,d,n-1);
         reverse(arr,0,n-1);
